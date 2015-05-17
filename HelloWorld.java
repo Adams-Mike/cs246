6b04dad23de5eab536273e123c798ae757f3a699
Mike Adams <magnavoid@gmail.com>
2015-05-16 19:29:10 -0600

    Edited HelloWorld.java - Displays: Hello GitHub World to console.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 /**
  * The HelloWorld class implements an application that
- * simply prints "Hello World!" to standard output.
+ * simply prints "Hello GitHub World" to standard output.
  */
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World"); // Display the string.
+        System.out.println("Hello GitHub World"); // Display the string.
     }
 }
