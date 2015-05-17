a2c707d0dbf95f39dcf52a1d744a025327d6148f
Mike Adams <magnavoid@gmail.com>
2015-05-16 19:33:36 -0600

    Edited HelloWorld.java - Now displays: Testing... as well as previous Hello GitHub World

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GitHub World"); // Display the string.
+        System.out.println("Testing...");
     }
 }
